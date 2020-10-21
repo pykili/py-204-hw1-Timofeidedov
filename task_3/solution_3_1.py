6bda614f2d230485f449d71f83497c79324e589c
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-21 22:08:03 +0300

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -1,4 +1,10 @@
-# your code here
-user_input = input()
-# your code here
+user_input=input()
+alphabet=''
+i=0
+while i<len(user_input):
+  if user_input[i] in alphabet :
+    alphabet=alphabet
+  else:
+    alphabet=alphabet+user_input[i]
+  i=i+1
 print(alphabet)
