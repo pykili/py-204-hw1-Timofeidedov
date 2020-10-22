46336c22eb650f4d1c461135e493da606fb60a6f
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-22 11:49:32 +0300

    Update solution_4_1.py

--- task_4/solution_4_1.py
+++ task_4/solution_4_1.py
@@ -1,6 +1,24 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+n=0
+while n<=10:
+  a=input()
+  n=n+1
+  i=0
+  j=0
+  form=''
+  lemma=''
+  if a[0] != '#' and a[0] != ' ':
+    while a[i]!='\t':
+        i=i+1
+    i=i+1
+    j=i
+    while a[j]!='\t':
+        j=j+1
+    j=j+1
+    while i!='\t' and j!='\t':
+      form=form+a[i]
+      lemma=lemma+a[j]#он выдает тут string index out of range, я пока не могу понять почему, поэтому временно оставил так
+      i=i+1
+      j=j+1
+    if form!=lemma:
+      print (form)
+      print (lemma)
