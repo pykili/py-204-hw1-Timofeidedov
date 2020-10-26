20e3e736dccd4015c78fd8421b4da922a736adc2
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-26 15:14:36 +0300

    Update solution_11.py

--- task_11/solution_11.py
+++ task_11/solution_11.py
@@ -1,1 +1,28 @@
-# your code here
+a=input()
+i=0
+left=''
+right=''
+while a[i]!=' ':
+  left=left+a[i]
+  i=i+1
+i=i+1
+while i<len(a):
+  right=right+a[i]
+  i=i+1
+k=0
+n=0
+trueaf=''
+if len(left)<=len(right):
+  while k<len(left) and n<len(right):
+    if left[k]==right[n]:
+      trueaf=trueaf+left[k]
+      k=k+1
+      n=n+1
+    else:
+      n=n+1
+  if trueaf==left:
+    print('YES')
+  else:
+    print('NO')
+else:
+  print('NO')
