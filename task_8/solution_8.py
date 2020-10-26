192bb6ed223449a206518f1e81765f458619220b
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-26 18:45:51 +0300

    Update solution_8.py

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,10 +1,12 @@
 n=int(input())
 a=0
 i=0
+k=0
 while i<=n-1:
     user_input = int(input())
     if user_input==0:
-      break
+      i=n
     a=a + user_input
+    k=k+1
     i=i+1
-print(a/i)
+print(a/k) #это если мы считаем среднее арифметическое с нулем, если без нуля -- будет (a/(k-1))
