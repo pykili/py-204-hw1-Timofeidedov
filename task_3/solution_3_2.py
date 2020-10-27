024fd28f61acd01f6d6cb8fbb0470df7907287da
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-27 15:42:57 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -4,9 +4,7 @@ n=1
 while n<=10:
   user_input=input()
   while i<len(user_input):
-    if user_input[i] in alphabet :
-      alphabet=alphabet
-    else:
+    if user_input[i] not in alphabet :
       alphabet=alphabet+user_input[i]
     i=i+1
   i=0
