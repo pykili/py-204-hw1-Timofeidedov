494dfc6c019e0e7ce82be26fa5088a1e09404ea0
Timofeidedov <71925458+Timofeidedov@users.noreply.github.com>
2020-10-28 10:34:00 +0300

    Update solution_12.py

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,12 +1,12 @@
 a=input()
 i=0
 i1=1
-i2=2
-i3=3
+i2=1
+i3=2
 occured_twice= False
 while i1<len(a):
-  i2=i+2
-  i3=i+3
+  i2=i+1
+  i3=i+2
   while i3<len(a):
     if a[i2]==a[i] and a[i3]==a[i1]:
   			occured_twice = True
